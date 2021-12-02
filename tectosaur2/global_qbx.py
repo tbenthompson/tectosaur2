Working on plane strain. Also fixing the signs on the Laplace kernels.

tectosaur2/global_qbx.py
@@ -16,13 +16,13 @@ def deriv_expand(exp_centers, src_pts, src_normals, r, m):
     w = src_pts[None, :, 0] + src_pts[None, :, 1] * 1j
     z0 = exp_centers[:, 0, None] + exp_centers[:, 1, None] * 1j
     nw = src_normals[None, :, 0] + src_normals[None, :, 1] * 1j
-    return nw * (r[:, None] ** m) / ((2 * np.pi) * (w - z0) ** (m + 1))
+    return -nw * (r[:, None] ** m) / ((2 * np.pi) * (w - z0) ** (m + 1))
 
 
 def base_eval(obs_pts, exp_centers, r, m):
     z = obs_pts[:, 0] + obs_pts[:, 1] * 1j
     z0 = exp_centers[:, 0] + exp_centers[:, 1] * 1j
-    return ((z - z0) ** m / (r ** m))[:, None]
+    return -((z - z0) ** m / (r ** m))[:, None]
 
 
 def deriv_eval(obs_pts, exp_centers, r, m):

tectosaur2/integrate.py
@@ -51,7 +51,8 @@ def integrate_term(
             np.asarray(singularities, dtype=np.float64)
         )
 
-    # step 1: figure out which observation points need to use QBX
+    # step 1: figure out which observation points need to use QBX and which need
+    # to use nearfield integration
     src_tree = scipy.spatial.KDTree(combined_src.pts)
     closest_dist, closest_idx = src_tree.query(obs_pts)
     closest_panel_length = combined_src.panel_length[

tectosaur2/laplace2d.py
@@ -2,6 +2,17 @@ import numpy as np
 
 from .integrate import Kernel
 
+"""
+Facts to note if you are comparing the formulas here with formulas in another
+place:
+
+1. The dx and dy terms are (observation minus source). Many sources use (source
+minus observation). This can introduce sign errors.
+
+2. The normal vectors are inward pointing normal vectors. Many sources use
+outward pointing normal vectors. This can introduce sign errors.
+"""
+
 
 class SingleLayer(Kernel):
     name = "single_layer"
@@ -16,7 +27,7 @@ class SingleLayer(Kernel):
         too_close = r2 <= 1e-16
         r2[too_close] = 1
 
-        G = (1.0 / (4 * np.pi)) * np.log(r2)
+        G = -(1.0 / (4 * np.pi)) * np.log(r2)
         G[too_close] = 0
 
         return (G * src.jacobians * src.quad_wts[None, :])[:, None, :, None]
@@ -84,7 +95,7 @@ class Hypersingular(Kernel):
         r2[too_close] = 1
 
         A = 2 * (dx * src.normals[None, :, 0] + dy * src.normals[None, :, 1]) / r2
-        C = 1.0 / (2 * np.pi * r2)
+        C = -1.0 / (2 * np.pi * r2)
         C[too_close] = 0
 
         out = np.empty((obs_pts.shape[0], 2, src.n_pts, 1))

tests/test_integrals.py
@@ -16,7 +16,13 @@ from tectosaur2.laplace2d import (
     double_layer,
     hypersingular,
 )
-from tectosaur2.mesh import apply_interp_mat, unit_circle, upsample
+from tectosaur2.mesh import (
+    apply_interp_mat,
+    barycentric_deriv,
+    build_interp_wts,
+    unit_circle,
+    upsample,
+)
 
 
 def elasticU(**kwargs):
@@ -45,6 +51,16 @@ kernel_types = [
     elasticA,
     elasticH,
 ]
+
+
+def test_barycentric():
+    qx, qw = gauss_rule(5)
+    interp_wts = build_interp_wts(qx)
+    f = lambda x: x ** 2
+    eval_x = np.linspace(-1, 1, 100)
+    deriv = barycentric_deriv(eval_x, qx, interp_wts, f(qx))
+    correct = 2 * eval_x
+    np.testing.assert_allclose(deriv, correct)
 
 
 @pytest.mark.parametrize("K_type", kernel_types)
@@ -209,28 +225,31 @@ def test_safety_mode():
     # perfect step function in stress.
     t = sp.var("t")
     surf = refine_surfaces(
-        [(t, t, 0 * t)], gauss_rule(12), control_points=[(0, 0, 0, 0.1)]
+        [(t, t, 0 * t)], gauss_rule(12), control_points=[(0, 0, 0, 0.2)]
     )
-    print(surf.n_pts)
     displacement = 1 - np.abs(surf.pts[:, 0])
     mat = integrate_term(
         hypersingular,
         surf.pts,
         surf,
-        tol=1e-12,
+        tol=1e-11,
         safety_mode=True,
-        singularities=[(-1, 0), (0, 1)],
+        singularities=[(-1, 0), (1, 0)],
     )
-    stress = tensor_dot(mat, displacement)
-    import matplotlib.pyplot as plt
+    stress = -2 * tensor_dot(mat, displacement)
 
-    plt.figure(figsize=(8, 4))
-    plt.subplot(1, 2, 1)
-    plt.plot(surf.pts[:, 0], displacement)
-    plt.subplot(1, 2, 2)
-    plt.plot(surf.pts[:, 0], stress[:, 0])
-    plt.plot(surf.pts[:, 0], stress[:, 1])
-    plt.show()
+    # import matplotlib.pyplot as plt
+    # plt.figure(figsize=(8, 4))
+    # plt.subplot(1, 2, 1)
+    # plt.plot(surf.pts[:, 0], displacement)
+    # plt.subplot(1, 2, 2)
+    # plt.plot(surf.pts[:, 0], stress[:, 0])
+    # plt.plot(surf.pts[:, 0], stress[:, 1])
+    # plt.show()
+    # plt.plot()
+    # plt.plot(stress[:,0]+np.sign(surf.pts[:,0]) * 0.5)
+    # plt.show()
+    np.testing.assert_allclose(stress[:, 0], -np.sign(surf.pts[:, 0]))
 
 
 def test_fault_surface():
@@ -252,7 +271,7 @@ def test_fault_surface():
     (C, D) = integrate_term(
         hypersingular, fault.pts, free, fault, tol=1e-10, singularities=singularities
     )
-    fault_stress = tensor_dot(C, surf_disp) + tensor_dot(D, slip)
+    fault_stress = -tensor_dot(C, surf_disp) - tensor_dot(D, slip)
 
     cmp_surf_disp = -np.arctan(-1 / free.pts[:, 0]) / np.pi
 
@@ -276,11 +295,11 @@ def test_fault_surface():
     ys = np.linspace(*zoomy, nobs)
     obs_pts = pts_grid(xs, ys)
     Ai, Bi = integrate_term(double_layer, obs_pts, free, fault)
-    interior_disp = Ai[:, 0, :, 0].dot(surf_disp) + Bi[:, 0, :, 0].dot(slip)
+    interior_disp = -Ai[:, 0, :, 0].dot(surf_disp) - Bi[:, 0, :, 0].dot(slip)
     obsx = obs_pts[:, 0]
     obsy = obs_pts[:, 1]
     analytical_disp = (
-        -1.0
+        1.0
         / (2 * np.pi)
         * (np.arctan((obsy + 1) / obsx) - np.arctan((obsy - 1) / obsx))
     )